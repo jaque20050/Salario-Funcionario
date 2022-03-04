1b22f3130570ac54a7031f3ea6fbae8fb6feec6a
jaque20050 <jaque20050@gmail.com>
2022-03-04 17:07:11 -0300

    JpaRepository - Manipulando Interface

---
+++ src/main/java/com/devsuperior/aularepository/AularepositoryApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.aularepository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AularepositoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AularepositoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/aularepository/AularepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.aularepository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AularepositoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
